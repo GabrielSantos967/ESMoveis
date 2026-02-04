eefeb4b2fccaa7ed51f8ef2375d1837dd003ad65
GabrielSantos967 <biel2013as@gmail.com>
2026-02-04 20:47:22 -0300

    Fixing image error

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
       <nav className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           <div className="text-2xl font-bold text-slate-800">
-            <img src='./img/logo.webp' width={150}></img>
+            <img src='/img/logo.webp' width={150}></img>
           </div>
 
           <div className="hidden md:flex items-center gap-8">
